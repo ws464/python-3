231144922cb77a6d53de05c660a6f6d81963d0af
Sassi <william.sassi@fiserv.com>
2023-11-24 10:50:17 -0500

    ex1

--- src/ex1.py
+++ src/ex1.py
@@ -1,14 +1,19 @@
 from ValidationException import ValidationException
+import csv
+import re
 
-
-def validate_file():
-    pass # TODO
-
+def validate_file(file_name):
+    with open(file_name) as csv_file:
+        csvFile = csv.DictReader(csv_file, delimiter=",")
+        for row in csvFile:
+            print(str(row))
+            if(not row.get(' Miles')[1:].isnumeric()):
+                raise ValidationException("Miles must be a positive integer!")
 
 
 def ex1():
     try:
-        validate_file("input.txt")
+        validate_file("files/input.txt")
     except ValidationException as ve:
         print(ve)
 
